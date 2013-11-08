5e5798c2783be20ec8f7f95890be5f39174a9352
Andreas Künsken <kuensken@physik.rwth-aachen.de>
2013-11-08 16:17:41 +0100

    Modified BuildFile to run L1TriggerDPGUpgrade code
    created DataAggregatorFile for L1TriggerDPGUpgrade code

---
+++ L1Analyzer/python/DataAggregatorL1TUpgrade.py
@@ -0,0 +1,84 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('DataAggregatorL1TUpgrade')
+
+
+process.load('Configuration.StandardSequences.Services_cff')
+#process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')			#originally included
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+#process.load('Configuration.Geometry.GeometryIdeal_cff')			#originally included
+process.load('Configuration.Geometry.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.load('L1TriggerDPGUpgrade.L1TMuon.L1TMuonTriggerPrimitiveProducer_cfi')
+process.load('L1TriggerDPGUpgrade.L1TMuon.L1CSCTFTrackConverter_cfi')
+process.load('L1TriggerDPGUpgrade.L1TMuon.L1DTTFTrackConverter_cfi')
+process.load('L1TriggerDPGUpgrade.L1TMuon.L1RPCTFTrackConverter_cfi')
+process.load('L1TriggerDPGUpgrade.L1TMuon.L1TMuonSimpleDeltaEtaHitMatcher_cfi')
+
+# Originally included
+# 
+#from Configuration.AlCa.GlobalTag import GlobalTag
+#process.GlobalTag = GlobalTag(process.GlobalTag, 'START53_V7A::All', '')
+
+#global Tag for the given source File
+process.GlobalTag.globaltag = 'DES17_62_V7::All'
+
+infile = []
+infile.append('file:0E84878D-1522-E311-B1DB-003048678FB8.root')
+
+#
+# Originally included source files
+#
+##################################
+#infile.append('file:SingleMuFlatPt_minusEta_1GeVto200GeV_GEN_SIM_DIGI_L1.root')
+#['file:SingleMuFlatPt_5GeVto200GeV_GEN_SIM_DIGI_L1.root']
+#['file:SingleMuFlatPt_minusEta_1GeVto200GeV_GEN_SIM_DIGI_L1.root']
+#infile.append('file:SingleMuFlatPt_plusEta_1GeVto200GeV_GEN_SIM_DIGI_L1.root')
+#infile.append('file:SingleMuFlatPt_plusEta_1GeVto200GeV_GEN_SIM_DIGI_L1_2.root')
+#infile.append('file:SingleMuFlatPt_minusEta_1GeVto200GeV_GEN_SIM_DIGI_L1_2.root')
+
+process.source = cms.Source(
+    'PoolSource',
+    fileNames = cms.untracked.vstring(infile)
+    )
+
+process.L1TMuonSeq = cms.Sequence( process.L1TMuonTriggerPrimitives +
+                                   process.L1CSCTFTrackConverter    +
+                                   process.L1DTTFTrackConverter     +
+                                   process.L1RPCTFTrackConverters   +
+                                   process.L1TMuonSimpleDeltaEtaHitMatcher
+				)
+
+process.L1TMuonPath = cms.Path(process.L1TMuonSeq)
+
+outCommands = cms.untracked.vstring('drop *')
+outCommands.append('keep *_genParticles_*_*')
+outCommands.append('keep *_simCsctfDigis_*_*')
+outCommands.append('keep *_simDttfDigis_*_*')
+outCommands.append('keep *_simRpcTriggerDigis_*_*')
+outCommands.append('keep *_simMuonRPCDigis_*_*')
+outCommands.append('keep *_simDtTriggerPrimitiveDigis_*_*')
+outCommands.append('keep *_simCscTriggerPrimitiveDigis_*_*')
+outCommands.append('keep *_L1TMuonTriggerPrimitives_*_*')
+outCommands.append('keep *_*Converter_*_*')
+outCommands.append('keep *_*Matcher_*_*')
+
+process.FEVTDEBUGoutput = cms.OutputModule(
+    "PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = outCommands,
+    fileName = cms.untracked.string('L1TMuon.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('')
+    )
+)
+
+process.outPath = cms.EndPath(process.FEVTDEBUGoutput)
+
+process.schedule = cms.Schedule(process.L1TMuonPath,
+                                process.outPath)
