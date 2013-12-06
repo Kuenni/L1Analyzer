8839940d51dadc209210527c7f32d64d05674503
Andreas Künsken <kuensken@physik.rwth-aachen.de>
2013-12-06 15:12:17 +0100

    Added the missing module for the GenParticle muon filter already referenced by the python file for the DataAggregator

---
+++ L1Analyzer/python/GenParticleFilter_cfi.py
@@ -0,0 +1,11 @@
+import FWCore.ParameterSet.Config as cms
+
+#filter on the genparticles given in the gen particle id vector
+#filtering criteria are the min pt, status and eta
+mugenfilter = cms.EDFilter("MCSingleParticleFilter",
+	Status = cms.untracked.vint32(1,1),
+	MinPt = cms.untracked.vdouble(10.0,10.0),
+	ParticleID = cms.untracked.vint32(13,-13),
+	MinEta = cms.untracked.vdouble(-1.5,-1.5),
+	MaxEta = cms.untracked.vdouble(1.5,1.5),
+)
