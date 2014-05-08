e54a8f7b34af0e9e109c4f7fcec7bd91a07d3938
Andreas Künsken <kuensken@physik.rwth-aachen.de>
2014-05-08 15:53:31 +0200

    Added delta phi gun crab templatesx

---
+++ results/deltaPhiGun/DeltaPhiGun_template.py
@@ -0,0 +1,116 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.20 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: SingleMuPt10_cfi.py --step GEN,SIM,DIGI --geometry Extended2017 --beamspot Gauss --conditions auto:upgrade2017 --customise SLHCUpgradeSimulations/Configuration/combinedCustoms.cust_2017 --eventcontent FEVTDEBUG --datatier GEN-SIM-DIGI -n 100 --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('DIGI')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.Geometry.GeometryExtended2017Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2017_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedGauss_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.20 $'),
+    annotation = cms.untracked.string('DeltaPhiPt@pt@_cfi.py nevts:100'),
+    name = cms.untracked.string('Applications')
+)
+
+# Output definition
+
+process.FEVTDEBUGoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.FEVTDEBUGEventContent.outputCommands,
+    fileName = cms.untracked.string('DeltaPhiGun.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM-DIGI')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgrade2017', '')
+
+process.generator = cms.EDProducer("DeltaPhiGunProducer",
+    PGunParameters = cms.PSet(
+        MaxPt = cms.double(@pt_max@),
+        MinPt = cms.double(@pt_min@),
+        PartID = cms.vint32(-13),
+        MaxEta = cms.double(0.8),
+        MaxPhi = cms.double(3.14159265359),
+        MinEta = cms.double(-0.8),
+        MinPhi = cms.double(-3.14159265359),
+        InConeID = cms.vint32(-13),
+        MinMomRatio = cms.double(0.999999),
+        MaxMomRatio = cms.double(1.000001),
+        InConeMaxTry = cms.uint32(100),
+        MinDeltaPhi = cms.double(@dPhi_min@),
+        MaxDeltaPhi = cms.double(@dPhi_max@),
+        ParticleMinEta = cms.double(-0.8),
+        ParticleMaxEta = cms.double(0.8),
+        ParticleMinPhi = cms.double(-3.14159265359),
+        ParticleMaxPhi = cms.double(3.14159265359)
+    ),
+    Verbosity = cms.untracked.int32(0),
+    psethack = cms.string('double mu pt @pt@'),
+    AddAntiParticle = cms.bool(True),
+    firstRun = cms.untracked.uint32(1)
+)
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.digitisation_step = cms.Path(process.pdigi)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.FEVTDEBUGoutput_step = cms.EndPath(process.FEVTDEBUGoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.digitisation_step,process.endjob_step,process.FEVTDEBUGoutput_step)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq 
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from SLHCUpgradeSimulations.Configuration.combinedCustoms
+from SLHCUpgradeSimulations.Configuration.combinedCustoms import cust_2017 
+
+#call to customisation function cust_2017 imported from SLHCUpgradeSimulations.Configuration.combinedCustoms
+process = cust_2017(process)
+
+# End of customisation functions
