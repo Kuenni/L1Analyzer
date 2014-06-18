c81bf9a346f81c81dc5f8e3f0520da8b9db96314
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2014-06-18 23:57:11 +0200

    Adapted some plotting in the template

--- results/deltaPhiGun/TrigTestPlots_template.py
+++ results/deltaPhiGun/TrigTestPlots_template.py
@@ -8,7 +8,6 @@ PlotStyle.setPlotStyle()
 
 file = TFile.Open("DeltaPhi_Analysed.root")
 
-h1dFiltBtiPerEvt = file.Get("dttriganalyzer/h1dFilteredBtiHitsPerEvt")
 
 h1dFiltBtiPerEvtId1Sl1 = file.Get("dttriganalyzer/h1dFilteredBtiHitsPerEvtId1Sl1")
 h1dFiltBtiPerEvtId1Sl2 = file.Get("dttriganalyzer/h1dFilteredBtiHitsPerEvtId1Sl2")
@@ -28,11 +27,11 @@ h1dFiltBtiPerEvtId1Sl1.SetLineWidth(3)
 h1dFiltBtiPerEvtId1Sl2.SetLineWidth(3)
 h1dFiltBtiPerEvtId2Sl1.SetLineWidth(3)
 h1dFiltBtiPerEvtId2Sl2.SetLineWidth(3)
-h1dFiltBtiPerEvt.SetLineWidth(3)
 h1dFiltBtiPerEvtBothIdSl1.SetLineWidth(3)
 
 canv = TCanvas("ID1",'ID1')
 canv.cd(1).SetLogy()
+
 h1dFiltBtiPerEvtId1Sl1.SetStats(0)
 h1dFiltBtiPerEvtId1Sl1.SetTitle('Number of BX filtered HTRG per Event for Stat. 1, Track Id 1')
 h1dFiltBtiPerEvtId1Sl1.GetXaxis().SetRangeUser(0,10)
@@ -47,16 +46,28 @@ canv.SaveAs("@sampleName@_h1dFilteredBtiHitsPerEvtID1.png")
 
 canv2 = TCanvas("ID2",'ID2')
 canv2.cd(1).SetLogy()
+stack = THStack('btiFilteredID1Stack','Number of BX filtered HTRG per Event for ID 2')
+
 h1dFiltBtiPerEvtId2Sl1.SetStats(0)
 h1dFiltBtiPerEvtId2Sl1.SetTitle('Number of BX filtered HTRG per Event for Stat. 1, Track Id 2')
 h1dFiltBtiPerEvtId2Sl1.GetXaxis().SetRangeUser(0,10)
-h1dFiltBtiPerEvtId2Sl1.Draw()
+#h1dFiltBtiPerEvtId2Sl1.Draw()
 h1dFiltBtiPerEvtId2Sl2.SetLineColor(ROOT.kRed)
-h1dFiltBtiPerEvtId2Sl2.Draw('Same')
+#h1dFiltBtiPerEvtId2Sl2.Draw('Same')
+
+stack.Add(h1dFiltBtiPerEvtId2Sl1)
+stack.Add(h1dFiltBtiPerEvtId2Sl2)
+
+stack.Draw()
+stack.GetXaxis().SetTitle(h1dFiltBtiPerEvtId2Sl1.GetXaxis().GetTitle())
+stack.GetYaxis().SetTitle(h1dFiltBtiPerEvtId2Sl1.GetYaxis().GetTitle())
+stack.GetXaxis().SetRangeUser(0,10)
+
 legend = TLegend(0.75,0.75,0.9,0.9)
 legend.AddEntry(h1dFiltBtiPerEvtId2Sl1,'SL 1','l')
 legend.AddEntry(h1dFiltBtiPerEvtId2Sl2,'SL 2','l')
 legend.Draw()
+
 canv2.SaveAs("@sampleName@_h1dFilteredBtiHitsPerEvtID2.png")
 
 ##Draw a histogram stack for all SL1 information in Station 1
@@ -118,9 +129,10 @@ canv3.Update()
 
 canv3.SaveAs("@sampleName@_h1dFilteredBtiHitsPerEvtSL1.png")
 
+print "Id 1 SL1: %.2f %%" % (h1dBtiPerEvtId1Sl1.Integral()/50000*100)
+print "Id 2 SL1: %.2f %%" % (h1dBtiPerEvtId2Sl1.Integral()/50000*100)
 print "Id 1 Filtered SL1: %.2f %%" % (h1dFiltBtiPerEvtId1Sl1.Integral()/50000*100)
-print "Id 1 Filtered SL2: %.2f %%" % (h1dFiltBtiPerEvtId1Sl2.Integral()/50000*100)
+#print "Id 1 Filtered SL2: %.2f %%" % (h1dFiltBtiPerEvtId1Sl2.Integral()/50000*100)
 print "Id 2 Filtered SL1: %.2f %%" % (h1dFiltBtiPerEvtId2Sl1.Integral()/50000*100)
-print "Id 2 Filtered SL2: %.2f %%" % (h1dFiltBtiPerEvtId2Sl2.Integral()/50000*100)
-print "Both IDs Filtered SL1: %.2f %%" % (h1dFiltBtiPerEvt.Integral()/50000*100)
+#print "Id 2 Filtered SL2: %.2f %%" % (h1dFiltBtiPerEvtId2Sl2.Integral()/50000*100)
 
